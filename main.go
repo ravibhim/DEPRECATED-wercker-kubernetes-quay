0929593173f68a2510301c835c1b06f123dcd65f
Ravi Bhim <ravi.bhim@yahoo.com>
2017-05-13 20:17:10 -0700

    Hyderabad

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func CityHandler(res http.ResponseWriter, req *http.Request) {
 			"Paris",
 			"Seoul",
 			"Austin",
+			"Hyderabad",
 		},
 	}
 	data, _ := json.MarshalIndent(citiesResponse, "", "  ")
